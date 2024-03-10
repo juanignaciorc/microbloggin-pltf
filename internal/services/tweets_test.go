17ece4f1a8871cd4c078ad75650cb9c4273c17fc
juanignaciorc <juanignacioroldan01@gmail.com>
2024-03-10 16:39:40 -0300

    refactor test to add error cases and make TTD tests

--- internal/services/tweets_test.go
+++ internal/services/tweets_test.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"errors"
 	"github.com/google/uuid"
 	"github.com/juanignaciorc/microbloggin-pltf/internal/domain"
 	mock_ports "github.com/juanignaciorc/microbloggin-pltf/mocks"
@@ -10,31 +11,61 @@ import (
 	"testing"
 )
 
-func TestTweetsService_CreateTweetOK(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	mockCtx := context.Background()
+func TestTweetsService_CreateTweet(t *testing.T) {
 	mockUUID := uuid.New()
-	mockTw := domain.Tweet{
-		ID:      mockUUID,
-		UserID:  mockUUID,
-		Message: "message mock",
+
+	type testCase struct {
+		name       string
+		mockInput  domain.Tweet
+		mockOutput domain.Tweet
+		mockErr    error
+		expected   domain.Tweet
+		wantErr    bool
 	}
 
-	mockRepo := mock_ports.NewMockTweetRepository(ctrl)
-	s := NewTweetsService(mockRepo)
-
-	mockRepo.
-		EXPECT().
-		CreateTweet(mockCtx, gomock.Any()).
-		Return(mockTw, nil)
-
-	got, err := s.CreateTweet(mockCtx, mockUUID, "message")
-	if err != nil {
-		t.Errorf("CreateTweet() error = %v, ", err)
-		return
+	tests := []testCase{
+		{
+			name:       "Success case",
+			mockInput:  domain.Tweet{UserID: mockUUID, Message: "message"},
+			mockOutput: domain.Tweet{ID: mockUUID, UserID: mockUUID, Message: "message"},
+			mockErr:    nil,
+			expected:   domain.Tweet{ID: mockUUID, UserID: mockUUID, Message: "message"},
+			wantErr:    false,
+		},
+		{
+			name:       "Repository error",
+			mockInput:  domain.Tweet{UserID: uuid.New(), Message: "message"},
+			mockOutput: domain.Tweet{},
+			mockErr:    errors.New("repository error"),
+			expected:   domain.Tweet{},
+			wantErr:    true,
+		},
 	}
-	if !reflect.DeepEqual(got, mockTw) {
-		t.Errorf("CreateTweet() got = %v, want %v", got, mockTw)
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockCtx := context.Background()
+			mockRepo := mock_ports.NewMockTweetRepository(ctrl)
+			s := NewTweetsService(mockRepo)
+
+			mockRepo.
+				EXPECT().
+				CreateTweet(mockCtx, tc.mockInput).
+				Return(tc.mockOutput, tc.mockErr)
+
+			got, err := s.CreateTweet(mockCtx, tc.mockInput.UserID, tc.mockInput.Message)
+
+			if (err != nil) != tc.wantErr {
+				t.Errorf("CreateTweet() error = %v, wantErr = %v", err, tc.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tc.expected) {
+				t.Errorf("CreateTweet() got = %v, want = %v", got, tc.expected)
+			}
+		})
 	}
 }
